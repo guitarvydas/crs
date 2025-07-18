afa2d307053dc853fdef9220c999961dd6af02f0
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-07-18 08:42:59 -0400

    begin

---
+++ b.py
@@ -0,0 +1,8 @@
+function B(in) {
+  if (in == q) {
+    call C(s)
+} else if (in == r) {
+    call C(t)
+} else {
+  FatalError()
+}
---
+++ c.py
@@ -0,0 +1,14 @@
+function C(in) {
+  if (in == q) {
+    output <- v
+ } else if (in == r) {
+    output <- w
+ } else if (in == s) {
+    output <- x
+ } else if (in == t) {
+    output <- y
+ } else if (in == u) {
+    output <- z
+} else {
+   FatalError ()
+}
