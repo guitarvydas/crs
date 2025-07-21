fdb7c3aa0975312def359962f0548e83702336d3
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-07-21 08:29:44 -0400

    WIP working on documenting L0/L1/L2

--- Makefile
+++ Makefile
@@ -1,5 +1,25 @@
-all:
+all: v0 v1 v2 
+
+init:
 	npm install
 	node pbp/das/das2json.mjs crs.drawio
 	rm -f out.*
-	python3 main.py . "q" main crs.drawio.json
+
+v0: init
+	@echo ''
+	@echo 'version 0'
+	python3 main.py . "q" v0 crs.drawio.json
+v1: init
+	@echo ''
+	@echo 'version 1'
+	python3 main.py . "q" v1 crs.drawio.json
+v2: init
+	@echo ''
+	@echo 'version 2'
+	python3 main.py . "q" v2 crs.drawio.json
+
+# WIP...
+L0: init
+	@echo ''
+	@echo 'version L0'
+	python3 main.py . "q" L0 crs.drawio.json
--- main.py
+++ main.py
@@ -6,13 +6,19 @@ import c
 import d
 
 try:
+    # argv[1] is the directory path for this project (i.e. "."), no trailing slash
+    # argv[2] is the string to be injected into the "" port of the system, once running
+    # argv[3] is the name of the Part ("main" in this case - the "main" tab of crs.drawio)
+    # argv[4:] are JSON files representing the Container parts of the system
+    # b.py, c.py, d.py are Leaf parts (written in Python in this case)
     [palette, env] = zd.initialize_from_files (sys.argv[1], sys.argv[4:])
     b.install (palette)
     c.install (palette)
     d.install (palette)
-    # the system is started, then argv[2] is injected into the system 
     top = zd.start (arg=sys.argv[2], part_name=sys.argv[3], palette=palette, env=env)
 except Exception as e:
+    # this stuff makes the exception output less verbose
+    #  in general we don't expect to get any exceptions, here, but, bugs and typos happen when humans write code
     _, _, tb = sys.exc_info()
     while tb.tb_next:
         tb = tb.tb_next
